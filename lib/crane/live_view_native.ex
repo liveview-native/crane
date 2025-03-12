aa3c276c657e89249ec344b413cb0e0d857fd36c
Brian Cardarella <bcardarella@gmail.com>
2025-03-12 17:49:19 -0400

    Ensure LVN interceptor works for Window

--- lib/crane/live_view_native.ex
+++ lib/crane/live_view_native.ex
@@ -1,5 +1,5 @@
 defmodule Crane.LiveViewNative do
-  alias Crane.Protos.Browser.{Request, Response}
+  alias Crane.Protos.Browser.{Window, Request, Response}
 
   def init(_) do
     []
@@ -7,6 +7,27 @@ defmodule Crane.LiveViewNative do
 
   def call(%Request{} = request, stream, next, _options) do
     case next.(request, stream) do
+      {:ok, stream, response} ->
+        {:ok, view_tree} = LiveViewNative.Template.Parser.parse_document(response.body)
+        stylesheets = Floki.find(view_tree, "Style") |> Floki.attribute("url")
+
+        view_trees = %{
+          "main" => to_proto(view_tree, "[data-phx-main] > *"),
+          "disconnected" => lifecycle_template(view_tree, "disconnected"),
+          "reconnecting" => lifecycle_template(view_tree, "reconnecting"),
+          "error" => lifecycle_template(view_tree, "error")
+        }
+
+        {:ok, stream, %Response{
+          response | view_trees: view_trees, stylesheets: stylesheets
+        }}
+      error ->
+        error
+    end
+  end
+
+  def call(%Window{} = window, stream, next, _options) do
+    case next.(window, stream) do
       {:ok, stream, response} ->
         {:ok, view_tree} = LiveViewNative.Template.Parser.parse_document(response.body)
         stylesheets = Floki.find(view_tree, "Style") |> Floki.attribute("url")
