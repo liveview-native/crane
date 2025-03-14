08a5bf6f8e6b2f143eacb7e88269245f94b7970d
Brian Cardarella <bcardarella@gmail.com>
2025-03-14 09:44:49 -0400

    Default debugger to 50011

--- lib/crane/application.ex
+++ lib/crane/application.ex
@@ -3,7 +3,7 @@ defmodule Crane.Application do
   def start(_type, _args) do
     children = [
       {Crane.Browser, []},
-      {Bandit, plug: Crane.Endpoint.Debugger},
+      {Bandit, plug: Crane.Endpoint.Debugger, port: 50011},
       {GRPC.Server.Supervisor, endpoint: Crane.Endpoint.GRPC, port: 50051, start_server: true}
     ]
 
