69b212b7d72a468fcf196f7b1f6b407f1981273b
Brian Cardarella <bcardarella@gmail.com>
2025-04-02 17:14:33 -0400

    Configurable ports

--- lib/crane/application.ex
+++ lib/crane/application.ex
@@ -1,18 +1,29 @@
 defmodule Crane.Application do
  use Application
 
-  @live_reload Application.compile_env(:phoenix_playground, :live_reload)
-
   def start(_type, _args) do
 
-   children = [
+    live_reload = Application.get_env(:phoenix_playground, :live_reload)
+
+    grpc_port = System.get_env("GRPC_PORT", "50051") |> make_integer()
+    phx_port = System.get_env("PHX_PORT", "4000") |> make_integer()
+
+    children = [
       {Crane, []},
-      {GRPC.Server.Supervisor, endpoint: Crane.Endpoint.GRPC, port: 50051, start_server: true},
-      {PhoenixPlayground, plug: Crane.Phoenix.Router, live_reload: @live_reload, open_browser: false}
+      {GRPC.Server.Supervisor, endpoint: Crane.Endpoint.GRPC, port: grpc_port, start_server: true},
+      {PhoenixPlayground, plug: Crane.Phoenix.Router, live_reload: live_reload, open_browser: false, port: phx_port}
     ]
 
     opts = [strategy: :one_for_one, name: __MODULE__]
     Supervisor.start_link(children, opts)
+  end
+
+  defp make_integer(integer) when is_integer(integer),
+    do: integer
+  defp make_integer(integer) when is_binary(integer) do
+    {integer, _} = Integer.parse(integer)
+
+    integer
   end
 end
 
