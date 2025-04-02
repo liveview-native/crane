1119e682af541cede6151632f275ca28dc1616db
Brian Cardarella <bcardarella@gmail.com>
2025-04-02 17:14:33 -0400

    Port from system env

--- lib/crane/application.ex
+++ lib/crane/application.ex
@@ -5,13 +5,23 @@ defmodule Crane.Application do
 
   def start(_type, _args) do
 
+    grpc_port = System.get_env("GRPC_PORT", "50051") |> make_integer()
+
     children = [
       {Crane.Browser, []},
-      {GRPC.Server.Supervisor, endpoint: Crane.Endpoint.GRPC, port: 50051, start_server: true},
+      {GRPC.Server.Supervisor, endpoint: Crane.Endpoint.GRPC, port: grpc_port, start_server: true},
       {PhoenixPlayground, plug: Crane.Phoenix.Router, live_reload: @live_reload, open_browser: false}
     ]
 
     opts = [strategy: :one_for_one, name: __MODULE__]
     Supervisor.start_link(children, opts)
+  end
+
+  defp make_integer(integer) when is_integer(integer),
+    do: integer
+  defp make_integer(integer) when is_binary(integer) do
+    {integer, _} = Integer.parse(integer)
+
+    integer
   end
 end
