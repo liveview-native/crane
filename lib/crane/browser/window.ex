622cd4112dcb358e0537ec68dfb49b09c232433f
Brian Cardarella <brian@dockyard.com>
2025-06-19 11:40:37 -0400

    Add document pid to payload

--- lib/crane/browser/window.ex
+++ lib/crane/browser/window.ex
@@ -180,7 +180,7 @@ defmodule Crane.Browser.Window do
       name: window.name,
       stylesheets: window.stylesheets,
       browser_name: window.browser_name,
-      view_trees: Map.drop(window.view_trees, [:document])
+      view_trees: Map.put(window.view_trees, :document, window.view_trees.document.pid)
     }
   end
 
